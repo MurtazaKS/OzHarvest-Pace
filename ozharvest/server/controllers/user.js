d80b33bf548ed9e4fc599e4ce0e9cd14e7266d1c
Troy Hinton <troy.hinton@students.mq.edu.au>
2024-04-13 23:04:43 +1000

    Fix user controller included models

--- ozharvest/server/controllers/user.js
+++ ozharvest/server/controllers/user.js
@@ -1,6 +1,5 @@
 const express = require('express') 
 const { check, validationResult} = require("express-validator");
-const Post = require("../models/post")
 const User = require("../models/user")
 
 const router = express.Router()
