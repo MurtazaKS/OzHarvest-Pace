49c3cd24d452a8192a424264fb8c93c559cec362
Murtaza Khozema Sherwala <murtaza.sherwala@gmail.com>
2024-04-18 22:58:14 +1000

    added nodemon and add New visitor page updated

--- ozharvest/src/components/AddNewVisitor.js
+++ ozharvest/src/components/AddNewVisitor.js
@@ -18,10 +18,46 @@ const AddNewVisitor = () => {
           padding: "20px",
           borderRadius: "10px",
         }}>
-        <TextField />
-        <TextField />
-        <TextField />
-        <TextField />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="firstname"
+          label="First Name"
+          name="firstname"
+          autoComplete="firstname"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="lastname"
+          label="Last Name"
+          name="lastname"
+          autoComplete="lastname"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="typeofdoc"
+          label="Type of Document"
+          name="typeofdoc"
+          autoComplete="typeofdoc"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="documentid"
+          label="Document ID"
+          name="documentid"
+          autoComplete="documentid"
+          autoFocus
+        />
       </Box>
     </Box>
   );
